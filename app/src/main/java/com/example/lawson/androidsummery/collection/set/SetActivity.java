b6e9fdcaa8b23d7f64feca306f45f3a13d4a2517
lujianzhi <lujianzhixiaoyao@163.com>
2016-08-23 21:02:48 +0800

    集合内容

---
+++ app/src/main/java/com/example/lawson/androidsummery/collection/set/SetActivity.java
@@ -0,0 +1,15 @@
+package com.example.lawson.androidsummery.collection.set;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lawson.androidsummery.R;
+
+public class SetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set);
+    }
+}
