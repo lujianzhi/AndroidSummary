00e4a9959566061e948e93c2b1b718ef713ad296
lujianzhi <lujianzhixiaoyao@163.com>
2016-08-24 21:02:50 +0800

    事件分发机制

---
+++ app/src/main/java/com/example/lawson/androidsummery/touchevent/MyLinearLayout.java
@@ -0,0 +1,16 @@
+package com.example.lawson.androidsummery.touchevent;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Administrator on 2016/8/24.
+ */
+public class MyLinearLayout extends LinearLayout {
+
+    public MyLinearLayout(Context context) {
+        super(context);
+    }
+
+
+}
