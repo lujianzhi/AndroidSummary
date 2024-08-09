381d0a708c3396180706e9740305530c738b73f5
lujianzhi <lujianzhixiaoyao@163.com>
2024-08-09 14:21:30 +0800

    feat: 添加其他功能

--- app/src/main/java/com/example/lawson/androidsummery/pulltofront/PullToFrontActivity.java
+++ app/src/main/java/com/example/lawson/androidsummery/pulltofront/PullToFrontActivity.java
@@ -1,8 +1,22 @@
 package com.example.lawson.androidsummery.pulltofront;
 
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.lawson.androidsummery.R;
+import com.example.lawson.androidsummery.utils.ShellUtils;
+
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +26,92 @@ import androidx.appcompat.app.AppCompatActivity;
  * DATE：2024/8/7
  */
 public class PullToFrontActivity extends AppCompatActivity {
+    private Handler mHandler = new Handler();
+    private Runnable getForegroundAppRunnable = new Runnable() {
+        @Override
+        public void run() {
+            getForegroundApp(PullToFrontActivity.this);
+            if (mHandler != null) {
+                mHandler.postDelayed(getForegroundAppRunnable, 2000);
+            }
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pull_to_front);
+        findViewById(R.id.pull_up).setOnClickListener(v ->
+                bringAppToFront(PullToFrontActivity.this, "com.guming.iot"));
+        findViewById(R.id.front_app).setOnClickListener(v ->
+                getForegroundApp(PullToFrontActivity.this));
+        findViewById(R.id.loop_front_app).setOnClickListener(v ->
+                mHandler.postDelayed(getForegroundAppRunnable, 2000));
+        findViewById(R.id.running_app).setOnClickListener(v -> getRunningApps());
+        findViewById(R.id.start_app).setOnClickListener(v -> startAPP());
+        findViewById(R.id.kill_app).setOnClickListener(v -> killAPP());
+    }
+
+    private void killAPP() {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ShellUtils.execCommand("kill $(pidof com.ian.testapp com.example.lawson.androidsummery)", true, true);
+            }
+        }, 2000);
+    }
+
+    private void startAPP() {
+        PackageManager packageManager = getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage("com.guming.iot");
+        if (intent == null) {
+            Toast.makeText(PullToFrontActivity.this, "未安装", Toast.LENGTH_LONG).show();
+            return;
+        }
+        startActivity(intent);
+    }
+
+    private void getRunningApps() {
+        ShellUtils.execCommand("ps | grep com.", true, true);
+    }
+
+    private void bringAppToFront(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        Intent launchIntent = pm.getLaunchIntentForPackage(packageName);
+        if (launchIntent != null) {
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+        } else {
+            // 处理没有找到应用的情况
+            Log.i("ian", "App not found");
+        }
+    }
+
+    private void getForegroundApp(Context context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+        String currentApp = null;
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        long time = System.currentTimeMillis();
+        // 获取最近一段时间内的应用使用状态
+        List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, 0, time);
+        if (appList != null && !appList.isEmpty()) {
+            SortedMap<Long, UsageStats> sortedMap = new TreeMap<>();
+            for (UsageStats usageStats : appList) {
+                sortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+            }
+            if (!sortedMap.isEmpty()) {
+                currentApp = sortedMap.get(sortedMap.lastKey()).getPackageName();
+            }
+        }
+        Log.i("ian", "currentApp - " + currentApp);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacks(getForegroundAppRunnable);
+        mHandler = null;
     }
 }
---
+++ app/src/main/java/com/example/lawson/androidsummery/utils/ShellUtils.java
@@ -0,0 +1,357 @@
+package com.example.lawson.androidsummery.utils;
+
+import android.os.Build;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author lijia
+ * @date 2023/8/19.
+ * description：
+ */
+public class ShellUtils {
+
+    public static final String COMMAND_SU       = "su";
+    public static final String COMMAND_SH       = "sh";
+    public static final String COMMAND_EXIT     = "exit\n";
+    public static final String COMMAND_LINE_END = "\n";
+
+    private static String CMD_PS = "ps ";
+    private static String CMD_PS_T = "ps -t ";
+    public static final String TAG = ShellUtils.class.getSimpleName();
+
+    private ShellUtils() {
+        throw new AssertionError();
+    }
+
+    /**
+     * check whether has root permission
+     *
+     * @return
+     */
+    public static boolean checkRootPermission() {
+        return execCommand("echo root", true, false).result == 0;
+    }
+
+    /**
+     * execute shell command, default return result msg
+     *
+     * @param command command
+     * @param isRoot whether need to run with root
+     * @return
+     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     */
+    public static CommandResult execCommand(String command, boolean isRoot) {
+        return execCommand(new String[] {command}, isRoot, true);
+    }
+    /**
+     * execute shell command, default return result msg
+     *
+     * @param command command
+     * @param isRoot whether need to run with root
+     * @return
+     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     */
+    public static CommandResult execCommand(String command, boolean isRoot,String splitStr) {
+        return execCommand(new String[] {command}, isRoot, true,splitStr);
+    }
+    /**
+     * execute shell commands, default return result msg
+     *
+     * @param commands command list
+     * @param isRoot whether need to run with root
+     * @return
+     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     */
+    public static CommandResult execCommand(List<String> commands, boolean isRoot) {
+        return execCommand(commands == null ? null : commands.toArray(new String[] {}), isRoot, true);
+    }
+
+    /**
+     * execute shell commands, default return result msg
+     *
+     * @param commands command array
+     * @param isRoot whether need to run with root
+     * @return
+     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     */
+    public static CommandResult execCommand(String[] commands, boolean isRoot) {
+        return execCommand(commands, isRoot, true);
+    }
+
+    /**
+     * execute shell command
+     *
+     * @param command command
+     * @param isRoot whether need to run with root
+     * @param isNeedResultMsg whether need result msg
+     * @return
+     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     */
+    public static CommandResult execCommand(String command, boolean isRoot, boolean isNeedResultMsg) {
+        return execCommand(new String[] {command}, isRoot, isNeedResultMsg);
+    }
+
+    /**
+     * execute shell commands
+     *
+     * @param commands command list
+     * @param isRoot whether need to run with root
+     * @param isNeedResultMsg whether need result msg
+     * @return
+     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     */
+    public static CommandResult execCommand(List<String> commands, boolean isRoot, boolean isNeedResultMsg) {
+        return execCommand(commands == null ? null : commands.toArray(new String[] {}), isRoot, isNeedResultMsg);
+    }
+
+    /**
+     * execute shell commands
+     *
+     * @param commands command array
+     * @param isRoot whether need to run with root
+     * @param isNeedResultMsg whether need result msg
+     * @return <ul>
+     *         <li>if isNeedResultMsg is false, {@link CommandResult#successMsg} is null and
+     *         {@link CommandResult#errorMsg} is null.</li>
+     *         <li>if {@link CommandResult#result} is -1, there maybe some excepiton.</li>
+     *         </ul>
+     */
+    public static CommandResult execCommand(String[] commands, boolean isRoot, boolean isNeedResultMsg) {
+        int result = -1;
+        if (commands == null || commands.length == 0) {
+            return new CommandResult(result, null, null);
+        }
+        Log.i(TAG,"执行shell指令:"+ Arrays.toString(commands));
+        Process process = null;
+        BufferedReader successResult = null;
+        BufferedReader errorResult = null;
+        StringBuilder successMsg = null;
+        StringBuilder errorMsg = null;
+
+        DataOutputStream os = null;
+        try {
+            process = Runtime.getRuntime().exec(isRoot ? COMMAND_SU : COMMAND_SH);
+            os = new DataOutputStream(process.getOutputStream());
+            for (String command : commands) {
+                if (command == null) {
+                    continue;
+                }
+
+                // donnot use os.writeBytes(commmand), avoid chinese charset error
+                os.write(command.getBytes());
+                os.writeBytes(COMMAND_LINE_END);
+                os.flush();
+            }
+            os.writeBytes(COMMAND_EXIT);
+            os.flush();
+
+            result = process.waitFor();
+            // get command result
+            if (isNeedResultMsg) {
+                successMsg = new StringBuilder();
+                errorMsg = new StringBuilder();
+                successResult = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                errorResult = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+                String s;
+                while ((s = successResult.readLine()) != null) {
+                    successMsg.append(s);
+                    successMsg.append("\n");
+                }
+                while ((s = errorResult.readLine()) != null) {
+                    errorMsg.append(s);
+                    successMsg.append("\n");
+                }
+                Log.i(TAG,"执行shell指令完成，指令:"+ Arrays.toString(commands) +" , 结果 successMsg:"+successMsg+",errorMsg:"+errorMsg.toString());
+            }
+            Log.i(TAG,"执行shell指令完成，指令:"+ Arrays.toString(commands));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+                if (successResult != null) {
+                    successResult.close();
+                }
+                if (errorResult != null) {
+                    errorResult.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (process != null) {
+                process.destroy();
+            }
+        }
+        return new CommandResult(result, successMsg == null ? null : successMsg.toString(), errorMsg == null ? null
+                : errorMsg.toString());
+    }
+
+    /**
+     * execute shell commands
+     *
+     * @param commands command array
+     * @param isRoot whether need to run with root
+     * @param isNeedResultMsg whether need result msg
+     * @return <ul>
+     *         <li>if isNeedResultMsg is false, {@link CommandResult#successMsg} is null and
+     *         {@link CommandResult#errorMsg} is null.</li>
+     *         <li>if {@link CommandResult#result} is -1, there maybe some excepiton.</li>
+     *         </ul>
+     */
+    public static CommandResult execCommand(String[] commands, boolean isRoot, boolean isNeedResultMsg,String splitStr) {
+        int result = -1;
+        if (commands == null || commands.length == 0) {
+            return new CommandResult(result, null, null);
+        }
+        Log.i(TAG,"执行shell指令:"+ Arrays.toString(commands));
+        Process process = null;
+        BufferedReader successResult = null;
+        BufferedReader errorResult = null;
+        StringBuilder successMsg = null;
+        StringBuilder errorMsg = null;
+
+        DataOutputStream os = null;
+        try {
+            process = Runtime.getRuntime().exec(isRoot ? COMMAND_SU : COMMAND_SH);
+            os = new DataOutputStream(process.getOutputStream());
+            for (String command : commands) {
+                if (command == null) {
+                    continue;
+                }
+
+                // donnot use os.writeBytes(commmand), avoid chinese charset error
+                os.write(command.getBytes());
+                os.writeBytes(COMMAND_LINE_END);
+                os.flush();
+            }
+            os.writeBytes(COMMAND_EXIT);
+            os.flush();
+
+            result = process.waitFor();
+            // get command result
+            if (isNeedResultMsg) {
+                successMsg = new StringBuilder();
+                errorMsg = new StringBuilder();
+                successResult = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                errorResult = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+                String s;
+                while ((s = successResult.readLine()) != null) {
+                    successMsg.append(s+splitStr);
+                }
+                while ((s = errorResult.readLine()) != null) {
+                    errorMsg.append(s+splitStr);
+                }
+                Log.i(TAG,"执行shell指令完成，结果 successMsg:"+successMsg.toString()+",errorMsg:"+errorMsg.toString());
+            }
+            Log.i(TAG,"执行shell指令完成，指令:"+commands);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+                if (successResult != null) {
+                    successResult.close();
+                }
+                if (errorResult != null) {
+                    errorResult.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (process != null) {
+                process.destroy();
+            }
+        }
+        return new CommandResult(result, successMsg == null ? null : successMsg.toString(), errorMsg == null ? null
+                : errorMsg.toString());
+    }
+
+    /**
+     * result of command
+     * <ul>
+     * <li>{@link CommandResult#result} means result of command, 0 means normal, else means error, same to excute in
+     * linux shell</li>
+     * <li>{@link CommandResult#successMsg} means success message of command result</li>
+     * <li>{@link CommandResult#errorMsg} means error message of command result</li>
+     * </ul>
+     *
+     * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2013-5-16
+     */
+    public static class CommandResult {
+
+        /** result of command **/
+        public int    result;
+
+        /** success message of command result **/
+        public String successMsg;
+
+        /** error message of command result **/
+        public String errorMsg;
+
+        public CommandResult(int result) {
+            this.result = result;
+        }
+
+        public CommandResult(int result, String successMsg, String errorMsg) {
+            this.result = result;
+            this.successMsg = successMsg;
+            this.errorMsg = errorMsg;
+        }
+    }
+    public static String getModel() {
+        return Build.MODEL;
+    }
+
+    public static String getCMD_PS(){
+
+        String mode = getModel().toLowerCase();
+        String cmd  = CMD_PS;
+        switch (mode){
+            case "gm328801":
+            case "gm328802":
+            case "rk3288":
+                cmd  = CMD_PS;
+                break;
+            case "rk356x":
+                cmd  = "ps -A ";
+                break;
+            default:
+                break;
+        }
+        return cmd;
+    }
+    public static String getCMD_PS_T(){
+        String mode = getModel().toLowerCase();
+        String cmd  = CMD_PS_T;
+        switch (mode){
+            case "gm328801":
+            case "gm328802":
+            case "rk3288":
+                cmd  = CMD_PS_T;
+                break;
+            case "rk356x":
+                cmd  = "ps -T ";
+                break;
+            default:
+                break;
+        }
+        return cmd;
+    }
+}
